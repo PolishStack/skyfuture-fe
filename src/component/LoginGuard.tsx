9d6ad75207f2440a55917a04557ce874d5acf1c9
Thanachart Sangmola <thanachart.farn@gmail.com>
2024-06-01 05:54:11 +0700

    Update ws path

--- src/component/LoginGuard.tsx
+++ src/component/LoginGuard.tsx
@@ -75,7 +75,7 @@ const LoginGuard = ({ children }: { children: ReactNode }) => {
         setRewardList(transactionList);
 
         // Initialize WebSocket connection
-        const wsURL = import.meta.env.VITE_WS_SERVER_URL;
+        const wsURL = import.meta.env.VITE_WS_SERVER_URL + "/ws?userId=" + id;
 
         if (wsURL) {
           let retryCount = 0;
