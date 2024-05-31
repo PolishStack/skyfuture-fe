aab61ffbd33477d875283c2d609412ba4c6f2307
HotWasabiSushi <76900409+CallMeWasabi@users.noreply.github.com>
2024-05-31 15:14:24 +0700

    Add modal in manage transaction page in admin feature

--- src/pages/admin/ManageTransactionPage.tsx
+++ src/pages/admin/ManageTransactionPage.tsx
@@ -5,6 +5,7 @@ import {
   Button,
   Card,
   Flex,
+  Modal,
   SimpleGrid,
   Stack,
   Text,
@@ -13,12 +14,19 @@ import { TransactionType } from "../../services/api/type";
 import Swal from "sweetalert2";
 import axios from "../../services/api";
 import { getToken } from "../../utils/helpers";
+import { useDisclosure } from "@mantine/hooks";
+import { User } from "../../features/user/type";
 
 const ManageTransactionPage = () => {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const [transactions, setTransactions] = useState<TransactionType[] | null>(
     []
   );
   const [callFetch, setCallFetch] = useState(false);
+  const [
+    openedApproveModal,
+    { open: openApproveModal, close: closeApproveModal },
+  ] = useDisclosure(false);
 
   const refetchTransaction = () => {
     setCallFetch(!callFetch);
@@ -86,6 +94,27 @@ const ManageTransactionPage = () => {
     }
   };
 
+  const handleApproveTransaction = async (transactinon: TransactionType) => {
+    try {
+      const token = getToken();
+      const res = await axios.get(`/users/${transactinon.userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const { result } = res.data
+      setSelectedUser(result)
+      openApproveModal();
+    } catch (err) {
+      console.log(err);
+      Swal.fire({
+        icon: "error",
+        text: "EN: Failed to load bank information",
+        confirmButtonColor: "#6ee3a5",
+      });
+    }
+  };
+
   return (
     <div>
       <Header title="Quản trị viên Quản lý giao dịch" />
@@ -120,8 +149,9 @@ const ManageTransactionPage = () => {
                   fullWidth
                   mt="md"
                   radius="md"
-                  onClick={() =>
-                    handleUpdateStatusTransaction(transaction, "success")
+                  onClick={() => 
+                    handleApproveTransaction(transaction)
+                    // handleUpdateStatusTransaction(transaction, "success")
                   }
                 >
                   Approve
@@ -142,6 +172,19 @@ const ManageTransactionPage = () => {
           ))}
         </SimpleGrid>
       </Stack>
+
+      <Modal
+        opened={openedApproveModal}
+        onClose={closeApproveModal}
+        title={<h3>EN: Approve user transaction</h3>}
+        centered
+      >
+        <Stack>
+          <Flex>
+            <h4>Bank name: {}</h4>
+          </Flex>
+        </Stack>
+      </Modal>
     </div>
   );
 };
