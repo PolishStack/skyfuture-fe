f499d98c892484c5d4a96cc04f6b0df1e02351bb
Thanachart Sangmola <thanachart.farn@gmail.com>
2024-05-31 18:38:30 +0700

    Remove unused import. Fix chat move up when typed

--- src/pages/CustomerServicePage.tsx
+++ src/pages/CustomerServicePage.tsx
@@ -5,18 +5,18 @@ const CustomerServicePage = () => {
   return (
     <>
       <Skeleton height="100vh" width="100%" pos="absolute" />
-      {/* BUG: everything move up when type something */}
       <iframe
         src={import.meta.env.VITE_CHAT_URL}
         style={{
           width: "calc(100% + 6px)",
           height: "calc(100vh - 60px - 38px)",
-          position: "absolute",
+          position: "fixed",
           top: "41px",
-          left: "-3px",
+          transform:"translateX(-3px)",
+          maxWidth:"546px"
         }}
       />
-      <Header title="Chăm Sóc Khách Hàng" />
+      <Header title="Chăm Sóc Khách Hàng" position="fixed" />
     </>
   );
 };
